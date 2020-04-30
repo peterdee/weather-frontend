6551cc4eca4bb0e9cf31cb3b25637a718030b31e
Peter <p.dyumin@dunice.net>
2020-04-30 23:53:16 +0300

    Styles

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,9 +1,14 @@
 import React, { memo } from 'react';
 
-import './style.scss';
-
-export default memo((): React.ReactElement => (
-  <div className="about">
-    <h1>About</h1>
+const About: React.FC = () => (
+  <div className="flex direction-column content">
+    <h1 className="noselect">
+      About
+    </h1>
+    <div className="margin-top">
+      This is a frontend for the Moleculer microservices.
+    </div>
   </div>
-));
+);
+
+export default memo(About);
