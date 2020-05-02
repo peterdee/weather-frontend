149efa55e3f09e779a58cb632f36482996593338
Peter <p.dyumin@dunice.net>
2020-05-02 23:47:25 +0300

    Display the services status

--- src/views/Index/Index.tsx
+++ src/views/Index/Index.tsx
@@ -1,14 +1,30 @@
 import React, { memo, useEffect, useState } from 'react';
+import axios from 'axios';
 
 import './style.scss';
 
 const Index: React.FC = () => {
   const [databaseOnline, setDatabaseOnline] = useState(false);
+  const [databaseLoading, setDatabaseLoading] = useState(false);
   const [metaweatherOnline, setMetaweatherOnline] = useState(false);
+  const [metaweatherLoading, setMetaweatherLoading] = useState(false);
 
   useEffect(
     () => {
-
+      setDatabaseLoading(true);
+      setMetaweatherLoading(true);
+      axios({
+        method: 'GET',
+        url: 'http://localhost:5522/api/ping',
+      }).then(() => setDatabaseOnline(true))
+      .catch(() => setDatabaseOnline(false))
+      .finally(() => setDatabaseLoading(false));
+      axios({
+        method: 'GET',
+        url: 'http://localhost:5544/api/ping',
+      }).then(() => setMetaweatherOnline(true))
+      .catch(() => setMetaweatherOnline(false))
+      .finally(() => setMetaweatherLoading(false));
     },
     [],
   );
@@ -16,6 +32,18 @@ const Index: React.FC = () => {
   return (
     <div className="flex direction-column content">
       <h1>MetaWeather</h1>
+      <div className="margin-top">
+        { `Database microservice: ${!databaseLoading && databaseOnline
+          ? 'online'
+          : 'offline'}`
+        }
+      </div>
+      <div className="margin-top">
+        { `MetaWeather API microservice: ${!metaweatherLoading && metaweatherOnline
+          ? 'online'
+          : 'offline'}`
+        }
+      </div>
     </div>
   );
 };
