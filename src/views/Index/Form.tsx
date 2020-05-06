afc9c99be9a18b441667a9d6c6f323a15d828dcd
Peter <p.dyumin@dunice.net>
2020-05-06 23:53:00 +0300

    Loading the data

--- src/views/Index/Form.tsx
+++ src/views/Index/Form.tsx
@@ -6,12 +6,14 @@ import './style.scss';
 type FormProps = {
   handleForm: React.FormEventHandler,
   handleInput: Function,
+  isEnabled: boolean,
   search: string,
-}
+};
 
 const Form: React.FunctionComponent<FormProps> = ({
   handleForm,
   handleInput,
+  isEnabled,
   search,
 }) => (
   <form onSubmit={handleForm}>
@@ -24,6 +26,7 @@ const Form: React.FunctionComponent<FormProps> = ({
     />
     <button
       className="margin-top"
+      disabled={!isEnabled}
       type="submit"
     >
       SEARCH
@@ -34,6 +37,7 @@ const Form: React.FunctionComponent<FormProps> = ({
 Form.propTypes = {
   handleForm: PropTypes.func.isRequired,
   handleInput: PropTypes.func.isRequired,
+  isEnabled: PropTypes.bool.isRequired,
   search: PropTypes.string.isRequired,
 };
 
--- src/views/Index/Index.tsx
+++ src/views/Index/Index.tsx
@@ -4,12 +4,21 @@ import axios from 'axios';
 import Form from './Form';
 import './style.scss';
 
+type LocationItem = {
+  latt_long: string,
+  location_type: string,
+  title: string,
+  woeid: number,
+};
+
 const Index: React.FC = () => {
   const [databaseOnline, setDatabaseOnline] = useState(false);
   const [databaseLoading, setDatabaseLoading] = useState(false);
   const [metaweatherOnline, setMetaweatherOnline] = useState(false);
   const [metaweatherLoading, setMetaweatherLoading] = useState(false);
 
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState('');
 
   useEffect(
@@ -42,21 +51,27 @@ const Index: React.FC = () => {
       event.preventDefault();
 
       // check if search is empty
-      if (!search) {
+      if (!search || (search && search.length === 1)) {
         return null;
       }
 
       // run the search
-      const response = await axios({
+      setLoading(true);
+      const { data: { data = [] } = {} } = await axios({
         method: 'GET',
         url: `http://localhost:5522/api/data/locations?query=${search}`,
       });
 
-      return console.log(response);
+      setLoading(false);
+      console.log(data)
+      setList(data);
     } catch(error) {
+      setLoading(false);
       return console.log(error);
     }
   }
+
+  const getDetails = (id: number) => console.log(id);
 
   return (
     <div className="flex direction-column content">
@@ -77,8 +92,18 @@ const Index: React.FC = () => {
         <Form
           handleForm={handleForm}
           handleInput={setSearch}
+          isEnabled={!loading}
           search={search}
         />
+      </div>
+      <div className="margin-top">
+        { list.map((location: LocationItem) => (
+          <div key={location.woeid}>
+            <button onClick={() => getDetails(location.woeid)}>
+              { location.title }
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
