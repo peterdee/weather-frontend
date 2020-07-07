38382f516966954cedbb234fd057b924cd66b892
Peter <p.dyumin@dunice.net>
2020-07-07 09:37:56 +0300

    Fix static serving

--- serve.js
+++ serve.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(helmet());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (_, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
